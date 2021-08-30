09540a1cab27b2efd855b8cfe628e6daa36c7b34
José Fernandes <jose-vitor-7@hotmail.com>
2021-08-30 02:16:22 -0300

    Add hamburger and header (no styling)

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Hamburger: React.FC = () => {
    return (
-      <div className="">menu</div>
+      <div className="hamburger-menu">
+         <div className="menu-secondary-background-color">
+
+         </div>
+         <div className="menu-layer">
+            <div className="menu-city-background">
+
+            </div>
+            <div className="container">
+               <div className="wrapper">
+                  <div className="menu-links">
+                     <nav>
+                        <ul>
+                           <li>
+                              <Link to="/opportunities">Opportunities</Link>
+                           </li>
+                           <li>
+                              <Link to="/solutions">Solutions</Link>
+                           </li>
+                           <li>
+                              <Link to="/contact">Contact</Link>
+                           </li>
+                        </ul>
+                     </nav>
+                     <div className="info">
+                        <h3>Our Promise</h3>
+                        <p>The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software.</p>
+                     </div>
+                     <div className="locations">
+                        Locations: 
+                        <span>Dallas</span>
+                        <span>Austin</span>
+                        <span>New York</span>
+                        <span>San Francisco</span>
+                        <span>Beijing</span>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
    )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { Hamburger } from './Hamburger'
 
 export const Header: React.FC = () => {
    return (
-      <header></header>
+      <header>
+         <div className="container">
+            <div className="wrapper">
+               <div className="inner-header">
+                  <div className="logo">
+                     <Link to="/">HAMBRG.</Link>
+                  </div>
+                  <div className="menu">
+                     <button>Menu</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <Hamburger />
+      </header>
    )
 }
