68438dd94f6bd82f4945a30c4dd657da112d8057
José Fernandes <jose-vitor-7@hotmail.com>
2021-08-30 03:57:56 -0300

    Add menu transition animations

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -1,30 +1,108 @@
 import React, { useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 
+import gsap from 'gsap'
+
 import { IHamburger } from '../interfaces/hamburger'
 
 export const Hamburger: React.FC<IHamburger> = ({ menuState }) => {
    const menu = useRef<HTMLDivElement>(null)
+   const revealMenu = useRef<HTMLDivElement>(null)
+   const revealMenuBackground = useRef<HTMLDivElement>(null)
+   const cityBackground = useRef<HTMLDivElement>(null)
+   const line1 = useRef<HTMLAnchorElement>(null)
+   const line2 = useRef<HTMLAnchorElement>(null)
+   const line3 = useRef<HTMLAnchorElement>(null)
+   const info = useRef<HTMLDivElement>(null)
+
+
+   const staggerHide = (node1: any, node2: any) => {
+      gsap.to([node1, node2], {
+         duration: .8,
+         height: 0,
+         ease: 'power3.inOut',
+         stagger: {
+            amount: .07
+         }
+      })
+   }
+
+   const staggerReveal = (node1: any, node2: any) => {
+      gsap.from([node1, node2], {
+         duration: .8,
+         height: 0,
+         transformOrigin: 'right top',
+         skewY: 2,
+         ease: 'power3.inOut',
+         stagger: {
+            amount: .1
+         }
+      })
+   }
+
+   const staggerText = (node1: any, node2: any, node3: any) => {
+      gsap.from([node1, node2, node3], {
+         duration: .8,
+         y: 100,
+         delay: .1,
+         ease: 'power3.inOut',
+         stagger: {
+            amount: .3
+         }
+      })
+   }
+
+   const fadeInUp = (node: any) => {
+      gsap.from(node, {
+         y: 60,
+         duration: 1,
+         delay: .2,
+         opacity: 0,
+         ease: 'power3.inOut'
+      })
+   }
 
    useEffect(() => {
       if(menuState.clicked || (menuState.clicked && !menuState.initial)){
          if(menu.current){
-            menu.current.style.display = 'block'
+            gsap.to(menu.current, {
+               duration: 0,
+               css: {
+                  display: 'block'
+               }
+            })
+
+            gsap.to([revealMenu.current, revealMenuBackground.current], {
+               duration: 0,
+               opacity: 1,
+               height: '100%'
+            })
+
+            staggerReveal(revealMenuBackground.current, revealMenu.current)
+            fadeInUp(info.current)
+            staggerText(line1.current, line2.current, line3.current)
          }
       } else if(!menuState.clicked){
          if(menu.current){
-            menu.current.style.display = 'none'
+            gsap.to(menu.current, {
+               duration: 1,
+               css: {
+                  display: 'none'
+               }
+            })
+
+            staggerHide(revealMenu.current, revealMenuBackground.current)
          }
       }
    }, [menuState])
 
    return (
       <div ref={menu} className="hamburger-menu">
-         <div className="menu-secondary-background-color">
+         <div ref={revealMenuBackground} className="menu-secondary-background-color">
 
          </div>
-         <div className="menu-layer">
-            <div className="menu-city-background">
+         <div ref={revealMenu} className="menu-layer">
+            <div ref={cityBackground} className="menu-city-background">
 
             </div>
             <div className="container">
@@ -33,17 +111,17 @@ export const Hamburger: React.FC<IHamburger> = ({ menuState }) => {
                      <nav>
                         <ul>
                            <li>
-                              <Link to="/opportunities">Opportunities</Link>
+                              <Link ref={line1} to="/opportunities">Opportunities</Link>
                            </li>
                            <li>
-                              <Link to="/solutions">Solutions</Link>
+                              <Link ref={line2} to="/solutions">Solutions</Link>
                            </li>
                            <li>
-                              <Link to="/contact">Contact</Link>
+                              <Link ref={line3} to="/contact">Contact</Link>
                            </li>
                         </ul>
                      </nav>
-                     <div className="info">
+                     <div ref={info} className="info">
                         <h3>Our Promise</h3>
                         <p>The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software.</p>
                      </div>
