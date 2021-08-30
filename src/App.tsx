4012974c2c78931a6fe2dd9c00139f523b4dacb2
José Fernandes <jose-vitor-7@hotmail.com>
2021-08-29 22:05:02 -0300

    Set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { Solutions } from './pages/Solutions'
+import { Contact } from './pages/Contact'
 
 export const App = () => {
 
   return (
-    <>Start</>
+    <BrowserRouter>
+      <div className="app">
+        <div className="container">
+          <div className="wrapper">
+            <div className="home">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/opportunities" component={Home} />
+                <Route exact path="/solutions" component={Solutions} />
+                <Route exact path="/contact" component={Contact} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
