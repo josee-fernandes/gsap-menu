ac27025ae2b90785c3cbbe60e533a54d41736405
José Fernandes <jose-vitor-7@hotmail.com>
2021-08-30 02:54:05 -0300

    Add menu state on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Hamburger } from './Hamburger'
 
+interface IMenu{
+   initial: boolean
+   clicked: boolean
+   name: string
+   
+}
+
 export const Header: React.FC = () => {
+   const [menu, setMenu] = useState<IMenu>({
+      initial: true,
+      clicked: false,
+      name: 'Menu'
+   })
+
+   const [disabled, setDisabled] = useState(false)
+
+   const handleMenu = () => {
+      disableMenu()
+
+      if(menu.initial){
+         setMenu({
+            initial: false,
+            clicked: true,
+            name: 'Close'
+         })
+      } else if(menu.clicked){
+         setMenu({ ...menu, clicked: false, name: 'Menu' })
+      } else if(!menu.clicked){
+         setMenu({ ...menu, clicked: true, name: 'Close' })
+      }
+   }
+
+   const disableMenu = () => {
+      setDisabled(true)
+
+      setTimeout(() => {
+         setDisabled(false)
+      }, 1200)
+   }
+
    return (
       <header>
          <div className="container">
@@ -13,7 +52,7 @@ export const Header: React.FC = () => {
                      <Link to="/">HAMBRG.</Link>
                   </div>
                   <div className="menu">
-                     <button>Menu</button>
+                     <button disabled={disabled} onClick={handleMenu}>{menu.name}</button>
                   </div>
                </div>
             </div>
